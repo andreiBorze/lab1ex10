6048d906fa008cfc8cb3fbda370e2c888a30caee
Borze Andrei <borzeandrew@gmail.com>
2022-11-26 20:59:48 +0200

    Add project files.

---
+++ lab1ex10/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace lab1ex10
+{
+    /*Scrieti un program care sa va afisa suma cifrelor ununi numar intreg citit de la tastatura*/
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Acest program va calcula suam cifrelor unui numar intreg. \n");
+
+            Console.WriteLine("Introduceti un numar intreg de 2 cifre:");
+            var nr = int.Parse(Console.ReadLine());
+
+            var sum = nr / 10 + nr%10;
+
+            Console.WriteLine("Suma cifrelor este: " + sum);
+        }
+    }
+}
